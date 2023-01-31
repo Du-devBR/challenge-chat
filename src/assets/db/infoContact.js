f0523edd139d7620ebe7cf7a1c72353d1f68c652
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-01-31 08:19:20 -0300

    adicionado mais dados ao arquivo infoContact.js

--- src/assets/db/infoContact.js
+++ src/assets/db/infoContact.js
@@ -1,38 +1,118 @@
 export const infoContact = [
   {
-    nome: 'camila',
-    picture: 'https://pps.whatsapp.net/v/t61.24694-24/321189497_188822440395119_3042720346348937263_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQK3W03sL12Up0Bs_McuS9GzYX0PhnOAg83VPRGs3jGLg&oe=63E54832',
+    nome: 'React',
+    picture: 'https://ionicframework.com/docs/icons/logo-react-icon.png',
     message: [{
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, et?',
-      hour: '21:00'
+      content: 'Salve dev, sou o React',
+      hour: '08:32'
+    },
+    {
+      content: 'Sou uma biblioteca do javascript, posso te ajudar a construir porjetos incriveis.',
+      hour: '08:33'
+    },
+    {
+      content: 'estou a disposição!!!',
+      hour: '08:34'
     }]
   },
   {
-    nome: 'Eduardo',
-    picture: 'https://pps.whatsapp.net/v/t61.24694-24/321189497_188822440395119_3042720346348937263_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQK3W03sL12Up0Bs_McuS9GzYX0PhnOAg83VPRGs3jGLg&oe=63E54832',
+    nome: 'Javascript',
+    picture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
     message: [{
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, et?',
-      hour: '03:00'
+      content: 'Eai dev du, blz?',
+      hour: '14:30'
+    },
+    {
+      content: 'Meu filho React conversou com vc?',
+      hour: '14:32'
+    },
+    {
+      content: 'Falei para ele que você está aprendendo muito sobre a nossa tecnologia.',
+      hour: '14:35'
+    },
+    {
+      content: 'qualquer duvida estou a disposição, blz',
+      hour: '14:36'
     }]
   },
   {
-    nome: 'Poka',
-    picture: 'https://pps.whatsapp.net/v/t61.24694-24/321189497_188822440395119_3042720346348937263_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQK3W03sL12Up0Bs_McuS9GzYX0PhnOAg83VPRGs3jGLg&oe=63E54832',
+    nome: 'Html',
+    picture: 'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_640.png',
     message: [{
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, et?',
+      content: 'Eai du, tudo bem?',
       hour: '20:00'
     },
     {
-      content: 'Lorem ',
-      hour: '22:30'
+      content: 'Depois precisamos conversar sobre acessibilidade nos projetos.',
+      hour: '20:31'
+    },
+    {
+      content: 'vamos fazer coisas incriveis, para todos',
+      hour: '20:33'
+    },
+    {
+      content: 'ate mais meu amigo.',
+      hour: '20:33'
     }]
   },
   {
-    nome: 'Tchuca',
-    picture: 'https://pps.whatsapp.net/v/t61.24694-24/321189497_188822440395119_3042720346348937263_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQK3W03sL12Up0Bs_McuS9GzYX0PhnOAg83VPRGs3jGLg&oe=63E54832',
+    nome: 'CSS',
+    picture: 'https://logospng.org/download/css-3/logo-css-3-2048.png',
     message: [{
-      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, et?',
-      hour: '08:00'
+      content: 'O meu amigo eduardo',
+      hour: '16:51'
+    },
+    {
+      content: 'Eai conseguiu centralizar aquela Div?',
+      hour: '16:52'
+    },
+    {
+      content: 'Flex box e grid são demais, né?',
+      hour: '16:56'
+    },
+    {
+      content: 'Vamos deixar as interfaces incriveis!',
+      hour: '16:57'
+    },]
+  },
+  {
+    nome: 'Spring boot',
+    picture: 'https://user-images.githubusercontent.com/33158051/103925017-e7673b80-50e4-11eb-9379-ceb82e3f382c.png',
+    message: [{
+      content: 'Oi eduardo, tudo bem?',
+      hour: '10:21'
+    },
+    {
+      content: 'Sei que meu pai Java pode ser dificil aprender.',
+      hour: '10:22'
+    },
+    {
+      content: 'Mas vc tem se saido muito bem em entender como construir umas apis e orientada a objeto.',
+      hour: '10:25'
+    },
+    {
+      content: 'Bora terminar mais uns projetos, que o frontend estão nos esperando!',
+      hour: '10:29'
+    }]
+  },
+  {
+    nome: 'Github',
+    picture: 'https://pbs.twimg.com/profile_images/1414990564408262661/r6YemvF9_400x400.jpg',
+    message: [{
+      content: 'Du-devBR, tudo bem?',
+      hour: '19:00'
+    },
+    {
+      content: 'Você subiu seu ultimo commit?',
+      hour: '19:00'
+    },
+    {
+      content: 'Achei ele aqui, estava preso na push da branch',
+      hour: '19:10'
+    },
+    {
+      content: 'ate daqui a pouco!',
+      hour: '19:11'
     }]
   },
 ]
