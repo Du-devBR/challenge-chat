4a4311f5d88714cb5ea0760b2931102f4eb029be
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-01-30 21:47:30 -0300

    docs: criado mais arquivos mock no infoContact.js

--- src/assets/db/infoContact.js
+++ src/assets/db/infoContact.js
@@ -1,7 +1,38 @@
-export const contact = [
+export const infoContact = [
   {
-    nome: '',
-    picture: '',
-    message: []
-  }
+    nome: 'camila',
+    picture: 'https://pps.whatsapp.net/v/t61.24694-24/321189497_188822440395119_3042720346348937263_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQK3W03sL12Up0Bs_McuS9GzYX0PhnOAg83VPRGs3jGLg&oe=63E54832',
+    message: [{
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, et?',
+      hour: '21:00'
+    }]
+  },
+  {
+    nome: 'Eduardo',
+    picture: 'https://pps.whatsapp.net/v/t61.24694-24/321189497_188822440395119_3042720346348937263_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQK3W03sL12Up0Bs_McuS9GzYX0PhnOAg83VPRGs3jGLg&oe=63E54832',
+    message: [{
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, et?',
+      hour: '03:00'
+    }]
+  },
+  {
+    nome: 'Poka',
+    picture: 'https://pps.whatsapp.net/v/t61.24694-24/321189497_188822440395119_3042720346348937263_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQK3W03sL12Up0Bs_McuS9GzYX0PhnOAg83VPRGs3jGLg&oe=63E54832',
+    message: [{
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, et?',
+      hour: '20:00'
+    },
+    {
+      content: 'Lorem ',
+      hour: '22:30'
+    }]
+  },
+  {
+    nome: 'Tchuca',
+    picture: 'https://pps.whatsapp.net/v/t61.24694-24/321189497_188822440395119_3042720346348937263_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdQK3W03sL12Up0Bs_McuS9GzYX0PhnOAg83VPRGs3jGLg&oe=63E54832',
+    message: [{
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, et?',
+      hour: '08:00'
+    }]
+  },
 ]
